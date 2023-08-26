ad44c1900a77d16d307c598d6510b7845af82346
Michael Barrera <soporte@michaelbarrera.dev>
2023-08-26 00:58:41 -0600

    Simplified plugin into one file, Woocommerce compatibility added

--- real-ip-revealer.php
+++ real-ip-revealer.php
@@ -14,19 +14,19 @@
  * Requires PHP: 7.4
  */
 
-
 // Avoid direct calls to this file
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-// Include the function files
-include 'includes/proxy-real-ip.php';
-include 'includes/display-ip-notification.php';
-include 'includes/add-dismiss-script.php';
-
-// Hook the 'proxy_real_ip' function to the 'init' action
-add_action('init', 'proxy_real_ip');
+// Detecta si WooCommerce está activo
+if (class_exists('WooCommerce')) {
+    // Si WooCommerce está activo, engancha tu función al proceso de pago de WooCommerce
+    add_action('woocommerce_checkout_order_processed', 'proxy_real_ip', 10, 0);
+} else {
+    // Si WooCommerce no está activo, engancha tu función a la acción 'init'
+    add_action('init', 'proxy_real_ip');
+}
 
 // Hook the 'display_ip_notification' function to the 'admin_notices' action
 add_action('admin_notices', 'display_ip_notification');
@@ -35,3 +35,71 @@ add_action('admin_notices', 'display_ip_notification');
 add_action('wp_ajax_real_ip_revealer_dismiss_notification', function() {
     update_option('real_ip_revealer_notification_dismissed', 'yes');
 });
+
+/**
+ * Function to get the real IP from the client when behind a proxy.
+ * It checks a list of headers in order and returns the first valid public IP found.
+ */
+function proxy_real_ip() {
+    $ip_addr = null;
+
+    // Define the order of the headers to be checked
+    $headers = ['HTTP_CF_CONNECTING_IP', 'X_REAL_IP', 'HTTP_X_REAL_IP', 'X_FORWARDED_FOR', 'HTTP_X_FORWARDED_FOR'];
+
+    foreach ($headers as $header) {
+        if (!empty($_SERVER[$header])) {
+            $ips = explode(',', $_SERVER[$header]);
+            foreach ($ips as $ip) {
+                $ip = trim($ip); // Remove any whitespace
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+                    $ip_addr = $ip;
+                    break 2; // Break the entire loop as soon as we get a valid public IP
+                }
+            }
+        }
+    }
+
+    if ($ip_addr !== null) {
+        $_SERVER['REMOTE_ADDR'] = $ip_addr;
+    } else {
+        error_log('Proxy Real IP: No valid IP found in headers');
+        $_SERVER['REMOTE_ADDR'] = $_SERVER['SERVER_ADDR']; // Fallback to server IP
+    }
+
+    return $_SERVER['REMOTE_ADDR']; // Return the client IP as seen by the server
+}
+
+/**
+ * Function to display the notification in the admin panel.
+ */
+function display_ip_notification() {
+    $ip = proxy_real_ip();
+
+    if ($ip !== $_SERVER['SERVER_ADDR']) {
+        $class = 'notice notice-success is-dismissible real-ip-revealer-notification';
+        $message = sprintf( __( 'The current user\'s IP address is: %s', 'real-ip-revealer' ), $ip );
+    } else {
+        $class = 'notice notice-error is-dismissible real-ip-revealer-notification';
+        $message = __( 'Could not retrieve the current user\'s IP address.', 'real-ip-revealer' );
+    }
+
+    // Add JavaScript to the footer to handle the dismissal
+    add_action('admin_footer', 'add_dismiss_script');
+
+    printf('<div class="%1$s" data-dismissible="disable-done-forever"><p>%2$s</p></div>', esc_attr($class), esc_html($message));
+}
+
+function add_dismiss_script() {
+    echo "
+    <script>
+    jQuery(document).on('click', '.real-ip-revealer-notification .notice-dismiss', function() {
+        jQuery.ajax({
+            url: ajaxurl,
+            data: {
+                action: 'real_ip_revealer_dismiss_notification'
+            }
+        });
+    });
+    </script>
+    ";
+}
